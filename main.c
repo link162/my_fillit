892a8133fbf6f4eb18fe2a3ddf5198fd42de3ea3
Ihor RUBAN <iruban@e2r14p3.unit.ua>
2018-11-19 15:03:16 +0200

    git ignore and gnl to main

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: ybuhai <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 12:48:39 by ybuhai            #+#    #+#             */
-/*   Updated: 2018/11/19 12:39:38 by ybuhai           ###   ########.fr       */
+/*   Updated: 2018/11/19 15:01:41 by iruban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
+#include <stdio.h>
 
 void	open_file(char *file_name)
 {
@@ -25,11 +26,14 @@ void	open_file(char *file_name)
 int		main(int argc, char **argv)
 {
 	int fd;
+	char *buf;
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
 		if (fd < 0)
 			ft_putendl("error");
+		while(get_next_line(fd, &buf))
+				printf("%s\n", buf);
 	}
 	else
 		ft_putendl("usage: ./fillit file_name");
